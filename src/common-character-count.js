a0f3c9149236f4fe74124f3f0b31c0d4ec35d5c6
Maria Rumyantseva <manechka96@list.ru>
2023-04-15 23:56:08 +0300

    feat: add tasks solution

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,19 +1,12 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * Given two strings, find the number of common characters between them.
- *
- * @param {String} s1
- * @param {String} s2
- * @return {Number}
- *
- * @example
- * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
- * Strings have 3 common characters - 2 "a"s and 1 "c".
- */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  for (let symbol of s1) {
+    if (s2.includes(symbol)) {
+      count++;
+      s2 = s2.slice(0, s2.indexOf(symbol)) + s2.slice(s2.indexOf(symbol) + 1);
+    }
+  }
+  return count;
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -1,30 +1,13 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-/**
- * There's a list of file, since two files cannot have equal names,
- * the one which comes later will have a suffix (k),
- * where k is the smallest integer such that the found name is not used yet.
- *
- * Return an array of names that will be given to the files.
- *
- * @param {Array} names
- * @return {Array}
- *
- * @example
- * For input ["file", "file", "image", "file(1)", "file"],
- * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
- *
- */
 function renameFiles(names) {
   let result = [];
   let map = new Map();
   for (let name of names) {
     if (!map.has(name)) {
-      result.push(name);
-      map.set(name, names.filter(item => item === name).length - 1);
+      map.set(name, 1);
+      result.push(!result.includes(name) ? name : `${name}(${map.get(name)})`);
     } else {
-      result.push(`${name}(${})`);
-      map.set(name, map.get(name)--);
+      result.push(`${name}(${map.get(name)})`);
+      map.set(name, map.get(name) + 1);
     }
   }
   return result;
